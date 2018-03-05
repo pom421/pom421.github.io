5ae2489bba5103535b111695d9bb6abea893bbc9
Po Mauguet <mauguet.po@gmail.com>
2018-03-05 10:21:59 +0100

    amélioration performance

--- CodingChallenges/CC_03_Snake_game/snake.js
+++ CodingChallenges/CC_03_Snake_game/snake.js
@@ -11,13 +11,10 @@ class Snake {
       this.tail.push("vector to add")
       this.growing = false
     }
-    
-    for (let i = this.tail.length - 1; i > 0; i--) {
-      this.tail[i] = this.tail[i - 1]
-    }
-    
-    //this.tail[0] = this.tail[0].copy().add(this.speed)
-    this.tail[0] = p5.Vector.add(this.tail[0], this.speed)
+
+    this.tail.unshift(p5.Vector.add(this.tail[0], this.speed))
+    this.tail.pop()
+
     this.tail[0].x = constrain(this.tail[0].x, 0, width - unit)
     this.tail[0].y = constrain(this.tail[0].y, 0, height - unit)
 
@@ -32,7 +29,7 @@ class Snake {
     this.tail.forEach(elt => {
       rect(elt.x, elt.y, unit, unit)
     })
-    
+
   }
 
   isEating(food) {
@@ -42,10 +39,10 @@ class Snake {
 
   isColliding() {
 
-    if (this.tail[0].x >= width || this.tail[0].x < 0 || 
+    if (this.tail[0].x >= width || this.tail[0].x < 0 ||
       this.tail[0].y >= height || this.tail[0].y < 0) {
-        return true
-      }
+      return true
+    }
 
     for (var i = 1; i < this.tail.length; i++) {
       if (this.tail[0].dist(this.tail[i]) === 0) {
