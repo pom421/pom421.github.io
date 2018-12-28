1257771cf53cf9fe8de267246bbfce95fc3f5146
Po Mauguet <mauguet.po@gmail.com>
2018-12-28 20:46:20 +0100

    ajout répertoire build

---
+++ CodingChallenges/React/carrousel/build/precache-manifest.eeb6316184bd5e5e931bea14d7752661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a7c9d0d73cde895b69",
+    "url": "/static/css/main.6e74767d.chunk.css"
+  },
+  {
+    "revision": "17a7c9d0d73cde895b69",
+    "url": "/static/js/main.17a7c9d0.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "413269f14d09572657b82d5f3401dad3",
+    "url": "/index.html"
+  }
+];
