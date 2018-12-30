06a538863580b0fec7c277420344836618ee576b
Po Mauguet <mauguet.po@gmail.com>
2018-12-30 14:12:22 +0100

    maj homepage package.json

---
+++ CodingChallenges/React/carrousel/build/precache-manifest.27091ae2dfdf81d3386cf67f7afcaee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42bf1095818e69b50b97",
+    "url": "/CodingChallenges/React/carrousel/build/static/css/main.6e74767d.chunk.css"
+  },
+  {
+    "revision": "42bf1095818e69b50b97",
+    "url": "/CodingChallenges/React/carrousel/build/static/js/main.42bf1095.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/CodingChallenges/React/carrousel/build/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "bfcc080a08ab3d20e2ea",
+    "url": "/CodingChallenges/React/carrousel/build/static/js/runtime~main.bfcc080a.js"
+  },
+  {
+    "revision": "501d0acce82b3477c89aaa205cdb92e4",
+    "url": "/CodingChallenges/React/carrousel/build/index.html"
+  }
+];
--- CodingChallenges/React/carrousel/build/service-worker.js
+++ CodingChallenges/React/carrousel/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eeb6316184bd5e5e931bea14d7752661.js"
+  "/CodingChallenges/React/carrousel/build/precache-manifest.27091ae2dfdf81d3386cf67f7afcaee0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CodingChallenges/React/carrousel/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
